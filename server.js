3f9f4f448eeea7bbef505c3f4118604b98361a08
YolandaMoreno <yolandamoreno1243@gmail.com>
2016-07-10 21:55:01 -0500

    server

--- server.js
+++ server.js
@@ -1,33 +1,12 @@
-//http
-var http= require("http");
-var fs= require('fs');
-var config= require("./conf/config.json")();
-//obteniendo las config
-//del modulo de conf
-var PORT = config.PORT;
-var IP = config.IP;
-if(IP==`127.0.0.1`){
-    Console.log("> --- EJECUTANDO EN MODO LOCAL ---");
-}
-//crear un servidor basico 
-var server= http.createServer(function(req, res){
-    //armar la respuesta en http
-    //armar un encabezado http
+var http = require("http");
+var sever = http.createServer(function(req, res){
     res.writeHead(200,{
-        "Content-Type": "text/plain",
-        "Server": "ITGAM@4.2.4"
+        "Content-Type" : "text/plain",
+        "Server" : "ITGAM@4.2.4"
     });
-    //lectura del archivo a servir
-    fs.readFile(`./static/index.html/`,`utf8`,
-    function(err,content){
-        if(err){
-            res.write("<h1>ERROR DE LECTURA </h1>");
-        }else{
-            res.end(content);
-        }
-    })
-});
-//poner a trabajar al server
-server.listen(PORT,IP,function(){
-    console.log(`> Server listening @http://${IP}:${PORT}...`)
+    res.write("HOLA MUNDO");
+    res.end();
+})
+server.listen(300,`127.0.01`, function(){
+    console.log("> Server listening @http://localhost:300...");
 });
