13dfeab2b25156ef7485bf5402afc7afeb531c26
YolandaMoreno <yolandamoreno1243@gmail.com>
2016-07-13 22:39:48 -0500

    pactica

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+//http
 var http = require("http");
-var sever = http.createServer(function(req, res){
-    res.writeHead(200,{
-        "Content-Type" : "text/plain",
+//obteniendo informacion del entorno
+// de ejecucion con respecto al Ip
+//y el puerto que devemos de usar en
+//nuestro server
+var PORT = process.env.PORT;
+var IP = process.env.IP || `127.0.0.1`;
+if(IP==`127.0.0.1`){
+    console.log("> ---- EJECUTANDO EN MODO LOCAL ----");
+}
+
+//crear un servidor basico
+var server = http.createServer(function(req,res){
+    //Armar la respuesta http
+    //Armar un encabezado http
+    res.writeHead(200, {
+        "Content-Type" : "text/html",
         "Server" : "ITGAM@4.2.4"
     });
-    res.write("HOLA MUNDO");
+    // Enviamos la respuesta
+    res.write("<h1>Yolanda Moreno server</h1>");
+    //cerrar conexion
     res.end();
-})
-server.listen(300,`127.0.01`, function(){
-    console.log("> Server listening @http://localhost:300...");
+});
+//poner atrabajar el server
+server.listen(PORT,IP,function(){
+  console.log(`> Server listening @http://${IP}:${PORT} ...`);
+
 });
